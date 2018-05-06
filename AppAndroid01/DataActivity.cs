30ef29f4b0728afbcb38ef4c9262e026fb24a2bb
korbek100 <korbek100@gmail.com>
2018-05-06 02:11:48 -0300

    first commit

---
+++ AppAndroid01/DataActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AppAndroid01
+{
+    [Activity(Label = "DataActivity")]
+    public class DataActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Data);
+
+            var TVNombre = FindViewById<TextView>(Resource.Id.DataTVNombre);
+
+            TVNombre.Text = Intent.GetStringExtra("nombre");
+
+
+            // Create your application here
+        }
+    }
+}
---
+++ AppAndroid01/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace AppAndroid01
+{
+    [Activity(Label = "AppAndroid01", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            button.Click += Send;
+
+        }
+
+        private void Send(object sender, EventArgs e)
+        {
+           var EtNombre =  FindViewById<EditText>(Resource.Id.MainETNombre);
+           var nombre = EtNombre.Text;
+            Intent intent = new Intent(this,typeof(DataActivity));
+            intent.PutExtra("nombre", nombre);
+            StartActivity(intent);
+           // throw new NotImplementedException();
+        }
+    }
+}
+
